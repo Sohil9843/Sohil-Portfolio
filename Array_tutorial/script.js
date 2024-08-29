3982494cabe7e8cdaa4657cc402445ac505ed1eb
Sohil9843 <sq551130@gmail.com>
2024-08-29 14:35:19 +0530

    new comits

---
+++ Array_tutorial/script.js
@@ -0,0 +1,62 @@
+// operators
+// logical operator
+// and operator
+// ===========================================
+
+// console.log('hello' && "Welcome")    o/p- Welcome
+// console.log('hello' && 15)           o/p- 15
+// console.log('' && 15)                o/p- blank
+
+// console.log(!!"Hello")               o/p- true
+// console.log(!!"")                    o/p- false
+// console.log(!"heloo")                o/p- false
+// console.log(!"")                     o/p- true
+
+// Logical OR operator
+
+// ===========================================
+
+// console.log('hello' || "Welcome")        o/p- hello
+// console.log('hello' || 15)               o/p- hello
+// console.log('' || 15)                    o/p- 15
+
+
+
+// ===========================================
+// typeof() oparator in JavaScript : describe the data type of variable or value 
+// let a = null
+// console.log(typeof(a))
+
+// ===========================================
+
+
+
+// calculator
+// let op = prompt("Enter the operaor:")
+// let num1 = prompt("Enter the 1st number:")
+// let num2 = prompt("Enter the 2nd number:")
+// let result;
+
+// if(op == "+"){
+//     result = num1+num2
+// }
+// else if(op == "-"){
+//     result = num1-num2
+// }
+// else if(op=="*"){
+//     result = num1*num2
+// }
+// else if(op=="/"){
+//     result=num1/num2
+// }
+// else{
+//     console.log(`${op} is invalid operator`)
+// }
+
+// console.log(`${num1} ${op} ${num2} = ${result}`)
+
+let calculate = (num1,num2,op)=>{
+    
+}
+
+
